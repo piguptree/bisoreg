149322456a31d11d05ed9cc24f36e14072d29076
Guo Zhu <30461435+piguptree@users.noreply.github.com>
2019-07-28 10:37:11 +0800

    Update fitted.biso.R

--- R/fitted.biso.R
+++ R/fitted.biso.R
@@ -10,14 +10,14 @@
   ## OUTPUTS:
   ## yhat = vector of predicted values
   ##
-  o <- order(object$x)
-  ro <- sapply(1:length(object$x), function(x) which(o==x))
-  x <- object$x[o]
-  y <- object$y[o]
+  if(missing(xnew)) xnew <- object$x
+  o <- order(xnew)
+  ro <- sapply(1:length(xnew), function(x) which(o==x))
+  xnew <- xnew[o]
   m <- object$m
   u <- as.matrix(object$postdraws[,1:m])
   u0 <- object$postdraws$u0
-  if(missing(xnew)) xnew <- x
+  
   wnew <- get.W((xnew-object$xmin)/object$xrng,m)
   yhat <- sapply(1:nrow(wnew),function(i) median(u0 + u%*%wnew[i,]))
   return(yhat[ro])
